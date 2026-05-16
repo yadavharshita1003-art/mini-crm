e228eca7b3fbefd33eb4dc34a2c06f260e0b47b5
HARSHITA YADAV <yadavharshita1003@gmail.com>
2026-05-16 13:06:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,26 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 const leadRoutes = require('./routes/leads');
 const authRoutes = require('./routes/auth');
+
 app.use('/api/leads', leadRoutes);
 app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
+
     console.log('MongoDB connected!');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+
   })
   .catch((err) => console.log(err));
